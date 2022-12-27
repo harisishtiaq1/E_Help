44df5cfe319777262a39a40876d547f9bc684002
Muzammili786 <awanmiqbal2016@gmail.com>
2022-12-27 22:36:08 +0500

    Login,Signup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Login from './Components/Login';
+import Signup from './Components/signup/signup';
 
 function App() {
   return (
-    <div className="App">
-    <Login/>
-    </div>
+    // <div className="App">
+    // <Login/>
+    // </div>
+    <>
+    <Router>
+    <Routes>
+      <Route path="/" element={<Login />}/>
+      <Route path="/Signup" element={<Signup />}/>
+    </Routes>
+    </Router>
+    </>
   );
 }
 
