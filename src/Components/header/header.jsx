e848efd71e9cacae2465251bcb429ec1fde7df65
awaiszafarofficial <75944655+awaiszafarofficial@users.noreply.github.com>
2022-12-28 17:23:24 +0500

    img

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -6,7 +6,9 @@ return(
 <div>
  <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">
+            <img src="" alt="" />
+        </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
